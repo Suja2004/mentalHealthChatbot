becec69e7c9b082e7d991d6b9e7179fcb006f029
Sujan Kumar K <sujan.22cs164@sode-edu.in>
2024-12-04 22:24:56 +0530

    health

--- backend/socket.js
+++ backend/socket.js
@@ -1,34 +1,34 @@
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 
-const setupSocket = (httpServer) => {
-    const io = new Server(httpServer, {
-        cors: {
-            origin: "https://mental-health-chatbot-hazel.vercel.app",
-            methods: ["GET", "POST"],
-        },
-    });
+// const setupSocket = (httpServer) => {
+//     const io = new Server(httpServer, {
+//         cors: {
+//             origin: "https://mental-health-chatbot-hazel.vercel.app",
+//             methods: ["GET", "POST"],
+//         },
+//     });
 
-    io.on("connection", (socket) => {
-        socket.on("joinRoom", ({ room, username }) => {
-            socket.join(room);
-            socket.to(room).emit("message", { sender: "System", text: `${username} has joined the room.` });
-        });
+//     io.on("connection", (socket) => {
+//         socket.on("joinRoom", ({ room, username }) => {
+//             socket.join(room);
+//             socket.to(room).emit("message", { sender: "System", text: `${username} has joined the room.` });
+//         });
 
-        socket.on("chatMessage", ({ room, message }) => {
-            io.to(room).emit("message", message);
-        });
+//         socket.on("chatMessage", ({ room, message }) => {
+//             io.to(room).emit("message", message);
+//         });
 
-        socket.on("leaveRoom", (room) => {
-            socket.leave(room);
-        });
+//         socket.on("leaveRoom", (room) => {
+//             socket.leave(room);
+//         });
 
-        socket.on("disconnect", () => {
-            // console.log("User disconnected");
-        });
-    });
+//         socket.on("disconnect", () => {
+//             // console.log("User disconnected");
+//         });
+//     });
 
 
-    return io;
-};
+//     return io;
+// };
 
-module.exports = setupSocket;
+// module.exports = setupSocket;
