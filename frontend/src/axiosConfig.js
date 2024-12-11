11193d45773032b848d68e21e50d977d58dd55e0
Sujan Kumar K <sujan.22cs164@sode-edu.in>
2024-12-11 22:38:59 +0530

    chatbot

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api', 
+    baseURL: 'https://mental-health-chatbot-server.vercel.app/api', 
 });
 
 axiosInstance.interceptors.response.use(
